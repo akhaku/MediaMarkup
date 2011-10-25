4febbb7f02d7bd039ef78686fc0325c585f62467
Seth Rattan <seth.rattan@gmail.com>
2011-10-25 19:42:27 -0400

    fixed browser issues

--- js/mm.js
+++ js/mm.js
@@ -1,3 +1,6 @@
+if( navigator.appCodeName == 'Mozilla' ) video = document.getElementById('video');
+/* Browser compatibility check */
+
 function getMin() {
     /* Get video.currentTime as minutes */
     var min = Math.floor(video.currentTime / 60);
@@ -12,18 +15,6 @@ function getSec() {
     return sec;
 }
 
-function togglePlay() {
-    /* Play video and toggle button to Pause */
-    video.play();
-    $('button#toggle').html("Pause");
-};
-
-function togglePause() {
-    /* Pause video and toggle button to Play */
-    video.pause();
-    $('button#toggle').html("Play");
-};
-
 $(document).ready(function() {
 
     $('video').bind('timeupdate',function() {
@@ -31,17 +22,10 @@ $(document).ready(function() {
         $('span#video-time').html(getMin() + ":" + getSec());
     });
 
-
-    $('#toggle').click(function() {
-        /* Toggle video between play and pause */
-        if( $('button#toggle').html() == 'Play' ) togglePlay();
-        else togglePause();
-    });
-
     $('#comment').click(function() {
         /* Open the comment entry dialog, pause the video */
-        if( $('button#toggle').html() == 'Pause' ) togglePause();
         if( $('textarea').get(0) ) return;
+        video.pause();
         $('div#controls').append(
             '<span id="comment-add">' +
                 '<textarea id="comment-text"></textarea>' +
@@ -50,6 +34,7 @@ $(document).ready(function() {
             '</span>'
         );
         $('textarea').focus();
+
         $('button#comment-save').click(function() {
             /* Display the comment and save it to the database */
             $('ul#master').append(
@@ -59,13 +44,16 @@ $(document).ready(function() {
                     '</span> - ' + $('#comment-text').val() +
                 '</li>'
             );
+
             $('span.timestamp').click(function() {
                 /* Jump the video to the timestamp */
                 if( $('button#toggle').html() == 'Pause' ) togglePause();
                 video.currentTime = $(this).attr('rel');
             });
+
             $('span#comment-add').remove();
         });
+
         $('button#comment-cancel').click(function() {
             /* Cancel the comment */
             $('span#comment-add').remove();
